6d7dbfaf200a6979e33b2044c4d62ff5453296ec
AleksandrovichK <killertom1997@gmail.com>
2018-12-31 00:09:35 +0300

    Annotations fixed

--- src/main/java/com/ibagroup/dao/IInscriptionDao.java
+++ src/main/java/com/ibagroup/dao/IInscriptionDao.java
@@ -1,11 +1,13 @@
 package com.ibagroup.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ibagroup.dto.Inscription;
 
 /**
  * @author DubininaE
  */
+@Repository
 public interface IInscriptionDao extends JpaRepository<Inscription, Long> {
 }
--- src/main/java/com/ibagroup/services/impl/TripService.java
+++ src/main/java/com/ibagroup/services/impl/TripService.java
@@ -2,20 +2,23 @@ package com.ibagroup.services.impl;
 
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.ibagroup.dao.ITripDao;
 import com.ibagroup.dto.Trip;
 import com.ibagroup.services.ITripService;
-
 /**
  * @author DubininaE
  */
+@Service
+@Transactional
 public class TripService implements ITripService {
     private final ITripDao dao;
 
     @Autowired
-
     public TripService(ITripDao dao) {
         this.dao = dao;
     }
